5130690ddf85616f75315ee5285a84e43ccb944d
Ian Matheus Veiga <162042603+IanMV@users.noreply.github.com>
2025-11-11 08:31:15 -0300

    Feat 2 (#4)

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { RouterView } from 'vue-router';
+import HeaderComponent from '@/components/HeaderComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
+</script>
+
+<template>
+  <HeaderComponent></HeaderComponent>
+  <main>
+    <RouterView></RouterView>
+  </main>
+  <FooterComponent/>
+</template>
---
+++ src/components/FooterComponent.vue
@@ -0,0 +1,27 @@
+<script setup>
+import { RouterLink } from "vue-router";
+import Logo from "@/../public/logo.svg";
+</script>
+
+<template>
+  <footer>
+    <RouterLink to="/">
+      <Logo />
+    </RouterLink>
+    <p>&copy; 2025 - Todos os direitos reservados</p>
+  </footer>
+</template>
+
+<style scoped lang="scss">
+footer {
+  color: c.$color-white;
+  text-align: center;
+  padding: 20px;
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+gap: 10px;
+
+}
+</style>
